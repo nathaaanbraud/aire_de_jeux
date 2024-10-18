450d387250323840f0111e821490d77f6b25f668
Jules Couplet <jules.couplet@etu.univ-tours.fr>
2024-10-18 16:03:09 +0200

    upt CTRUtilisateur

--- aire_de_jeux/src/main/java/com/example/aire_de_jeux/controllers/CTRUtilisateur.java
+++ aire_de_jeux/src/main/java/com/example/aire_de_jeux/controllers/CTRUtilisateur.java
@@ -1,7 +1,60 @@
 package com.example.aire_de_jeux.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.aire_de_jeux.dto.DTOUtilisateur;
+import com.example.aire_de_jeux.mappers.MAPUtilisateur;
+import com.example.aire_de_jeux.entities.Utilisateur;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/utilisateurs") // quand une recherche debutera par /api/utilisateurs, elle sera redirigée vers ce controller
 public class CTRUtilisateur {
+
+
+
+    @GetMapping  // Correction de l'annotation
+    public ResponseEntity<List<DTOUtilisateur>> getAllUtilisateurs() {
+        //TODO
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DTOUtilisateur> getUtilisateurById(@PathVariable Long id) {
+        //TODO
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<DTOUtilisateur> createUtilisateur(@RequestBody DTOUtilisateur dtoUtilisateur) {
+        //TODO
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public void updateUtilisateur(@PathVariable Long id, @RequestBody DTOUtilisateur dtoUtilisateur) {
+        //TODo
+    }
+
+    @DeleteMapping("/{id}")
+    public  void deleteUtilisateur(@PathVariable Long id) {
+        //TODO
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<DTOUtilisateur>> searchUtilisateur(@RequestParam String nom) {
+        //TODO
+        return null;
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<DTOUtilisateur>> searchUtilisateur(@RequestParam String nom, @RequestParam String prenom) {
+        //TODO
+        return null;
+    }
+
 }
