98ea73c15843b56ef8c517072c01ef2916b3e11e
nathaaanbraud <nathan.braud24@gmail.com>
2024-10-18 15:46:53 +0200

    Update CTRReservation

--- aire_de_jeux/src/main/java/com/example/aire_de_jeux/controllers/CTRReservation.java
+++ aire_de_jeux/src/main/java/com/example/aire_de_jeux/controllers/CTRReservation.java
@@ -1,4 +1,27 @@
 package com.example.aire_de_jeux.controllers;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/reservations")
 public class CTRReservation {
+
+    @PostMapping
+    public void addReservation(@RequestBody String reservation) {
+        // TODO: Implement this method
+    }
+
+    @DeleteMapping("/{reservation}")
+    public void deleteReservation(@PathVariable String reservation) {
+        // TODO: Implement this method
+    }
+
+    @GetMapping
+    public List<String> getAllReservations() {
+        // TODO: Implement this method
+        return new ArrayList<>();
+    }
 }
