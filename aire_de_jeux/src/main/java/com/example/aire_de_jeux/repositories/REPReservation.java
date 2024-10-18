8cde5ef91dcdc94c417b4e9b727aa8b11c8fc563
leaaah2 <rissel.lea@gmail.com>
2024-10-18 15:28:45 +0200

    repository jpa

--- aire_de_jeux/src/main/java/com/example/aire_de_jeux/repositories/REPReservation.java
+++ aire_de_jeux/src/main/java/com/example/aire_de_jeux/repositories/REPReservation.java
@@ -1,4 +1,6 @@
 package com.example.aire_de_jeux.repositories;
+import com.example.aire_de_jeux.entities.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class REPReservation {
+public interface REPReservation extends JpaRepository<Reservation, Long> {
 }
--- aire_de_jeux/src/main/java/com/example/aire_de_jeux/repositories/REPUtilisateur.java
+++ aire_de_jeux/src/main/java/com/example/aire_de_jeux/repositories/REPUtilisateur.java
@@ -1,4 +1,6 @@
 package com.example.aire_de_jeux.repositories;
+import com.example.aire_de_jeux.entities.Utilisateur;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class REPUtilisateur {
+public interface REPUtilisateur extends JpaRepository<Utilisateur, Long> {
 }
