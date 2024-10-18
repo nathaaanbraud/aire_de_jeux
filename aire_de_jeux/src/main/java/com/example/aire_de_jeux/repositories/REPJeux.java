b016040ee66cf8db2932dc9df843ffc943c615f8
leaaah2 <rissel.lea@gmail.com>
2024-10-18 15:36:00 +0200

    repository findbyid

--- aire_de_jeux/src/main/java/com/example/aire_de_jeux/repositories/REPJeux.java
+++ aire_de_jeux/src/main/java/com/example/aire_de_jeux/repositories/REPJeux.java
@@ -1,7 +1,11 @@
 package com.example.aire_de_jeux.repositories;
 import com.example.aire_de_jeux.entities.Jeux;
+import com.example.aire_de_jeux.entities.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface REPJeux extends JpaRepository<Jeux, Long> {
+    Optional<Jeux> findByID(Integer id);
 }
 
--- aire_de_jeux/src/main/java/com/example/aire_de_jeux/repositories/REPReservation.java
+++ aire_de_jeux/src/main/java/com/example/aire_de_jeux/repositories/REPReservation.java
@@ -1,6 +1,10 @@
 package com.example.aire_de_jeux.repositories;
 import com.example.aire_de_jeux.entities.Reservation;
+import com.example.aire_de_jeux.entities.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface REPReservation extends JpaRepository<Reservation, Long> {
+    Optional<Reservation> findByID(Integer id);
 }
